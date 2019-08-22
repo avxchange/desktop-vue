61ab9f3459c9dd6efeb5995a96b365639d5ada1b
avxchange <avxchangeio@gmail.com>
2019-08-22 16:34:27 +0800

    updated bootstrap - vue

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -3,8 +3,11 @@ import Router from 'vue-router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCog, faCodeBranch, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import BootstrapVue from 'bootstrap-vue'
 
 library.add(faCog, faCodeBranch, faCoffee)
+
+Vue.use(BootstrapVue)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
